e52a019d724b70b6fd5c8117a7bebe9acab64ef6
dev-alt <53392022+dev-alt@users.noreply.github.com>
2022-07-22 18:15:33 +1200

    Update part2.js

--- counter/part2.js
+++ counter/part2.js
@@ -1,18 +1,41 @@
 let saveEl = document.getElementById("saveEl");
 let countEl = document.getElementById("count-el");
-
+let totalEl = document.getElementById("total-el");
 let count = 0;
+let total = 0;
+
 function increment() {
-  count += 1;
-  countEl.innerText = count;
+  console.log("The increase button was clicked, the total is now: " + count);
+  count = count + 1;
+  updateCounter();
+}
+function decrement() {
+  console.log("The decrease button was clicked, the total is now: " + count);
+  count = count - 1;
+  console.log("The decrease button was clicked" + count);
+  updateCounter();
 }
 
 function save() {
+  total += count;
+  totalEl.textContent = "Total: " + total;
   let countString = count + " - ";
   saveEl.textContent += countString;
+  count = 0;
+  countEl.innerText = count;
+}
+function total() {
+  total += count;
+  totalEl.textContent = "Total: " + total;
+  count = 0;
+  countEl.innerText = count;
+}
+function updateCounter() {
+  document.getElementById("count-el").innerHTML = count;
 }
 function reset() {
   count = 0;
   saveEl.textContent = "";
   countEl.textContent = "";
+  total.textContent = "Total: ";
 }
